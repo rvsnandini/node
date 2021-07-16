c6aad77fbebf8220fcb8fefcc60726f2a56d8e67
rvsnandini <86845760+rvsnandini@users.noreply.github.com>
2021-07-16 10:12:57 +0530

    Add files via upload

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,27 @@
+import { MongoClient } from 'mongodb';
+
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      'mongodb+srv//newuser1:Narayana@179@cluster0.ng55v.mongodb.net/meetsups?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
+  }
+}
+
+export default handler;
